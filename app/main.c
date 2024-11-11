0689e9a5b3e90d205d27d164c8ab8dc515228376
471D38UNNUX <52738122+471D38UNNUX@users.noreply.github.com>
2024-11-12 02:26:29 +0700

    Update main.c

--- app/main.c
+++ app/main.c
@@ -1,1 +1,3 @@
-extern void ANativeActivity_onCreate();
+/* Don't remove this function except including this header
+ * #include <android/native_activity.h> */
+__attribute__((unused)) extern void ANativeActivity_onCreate();
