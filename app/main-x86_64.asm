69b61b67454adabd27f3f7eb79d0676efb53b51b
471D38UNNUX <aliwaskito123@gmail.com>
2024-12-09 12:29:48 +0700

    Update Repo

--- app/main-x86_64.asm
+++ app/main-x86_64.asm
@@ -5,7 +5,7 @@
 .section    .text
 
 ANativeActivity_onCreate:
-    sub         $8, %spl
+    sub         $8, %rsp
 
     mov         (%rdi), %rax
     lea         onStart(%rip), %rcx
@@ -46,36 +46,36 @@ ANativeActivity_onCreate:
     lea         Createt(%rip), %rdx
     call        __android_log_print
 
-    add         $8, %spl
+    add         $8, %rsp
 
     ret
 
 onStart:
-    sub         $8, %spl
+    sub         $8, %rsp
 
     mov         $ANDROID_LOG_INFO, %edi
     lea         LOG_TAG(%rip), %rsi
     lea         Startt(%rip), %rdx
     call        __android_log_print
 
-    add         $8, %spl
+    add         $8, %rsp
 
     ret
 
 onResume:
-    sub         $8, %spl
+    sub         $8, %rsp
 
     mov         $ANDROID_LOG_INFO, %rdi
     lea         LOG_TAG(%rip), %rsi
     lea         Resumet(%rip), %rdx
     call        __android_log_print
 
-    add         $8, %spl
+    add         $8, %rsp
 
     ret
 
 onSaveInstanceState:
-    sub         $8, %spl
+    sub         $8, %rsp
 
     movq        $4, (%rsi)
     mov         (%rsi), %rdi
@@ -92,168 +92,168 @@ onSaveInstanceState:
 
     mov         %rbx, %rax
 
-    add         $8, %spl
+    add         $8, %rsp
 
     ret
 error:
     mov         $1, %eax
 
-    add         $8, %spl
+    add         $8, %rsp
 
     ret
 
 onPause:
-    sub         $8, %spl
+    sub         $8, %rsp
 
     mov         $ANDROID_LOG_INFO, %rdi
     lea         LOG_TAG(%rip), %rsi
     lea         Pauset(%rip), %rdx
     call        __android_log_print
 
-    add         $8, %spl
+    add         $8, %rsp
 
     ret
 
 onStop:
-    sub         $8, %spl
+    sub         $8, %rsp
 
     mov         $ANDROID_LOG_INFO, %rdi
     lea         LOG_TAG(%rip), %rsi
     lea         Stopt(%rip), %rdx
     call        __android_log_print
 
-    add         $8, %spl
+    add         $8, %rsp
 
     ret
 
 onDestroy:
-    sub         $8, %spl
+    sub         $8, %rsp
 
     mov         $ANDROID_LOG_INFO, %rdi
     lea         LOG_TAG(%rip), %rsi
     lea         Destroyt(%rip), %rdx
     call        __android_log_print
 
-    add         $8, %spl
+    add         $8, %rsp
 
     ret
 
 onWindowFocusChanged:
-    sub         $8, %spl
+    sub         $8, %rsp
 
     mov         $ANDROID_LOG_INFO, %rdi
     lea         LOG_TAG(%rip), %rsi
     lea         WindowFocusChangedt(%rip), %rdx
     call        __android_log_print
 
-    add         $8, %spl
+    add         $8, %rsp
 
     ret
 
 onNativeWindowCreated:
-    sub         $8, %spl
+    sub         $8, %rsp
 
     mov         $ANDROID_LOG_INFO, %rdi
     lea         LOG_TAG(%rip), %rsi
     lea         NativeWindowCreatedt(%rip), %rdx
     call        __android_log_print
 
-    add         $8, %spl
+    add         $8, %rsp
 
     ret
 
 onNativeWindowResized:
-    sub         $8, %spl
+    sub         $8, %rsp
 
     mov         $ANDROID_LOG_INFO, %rdi
     lea         LOG_TAG(%rip), %rsi
     lea         NativeWindowResizedt(%rip), %rdx
     call        __android_log_print
 
-    add         $8, %spl
+    add         $8, %rsp
 
     ret
 
 onNativeWindowRedrawNeeded:
-    sub         $8, %spl
+    sub         $8, %rsp
 
     mov         $ANDROID_LOG_INFO, %rdi
     lea         LOG_TAG(%rip), %rsi
     lea         NativeWindowRedrawNeededt(%rip), %rdx
     call        __android_log_print
 
-    add         $8, %spl
+    add         $8, %rsp
 
     ret
 
 onNativeWindowDestroyed:
-    sub         $8, %spl
+    sub         $8, %rsp
 
     mov         $ANDROID_LOG_INFO, %rdi
     lea         LOG_TAG(%rip), %rsi
     lea         NativeWindowDestroyedt(%rip), %rdx
     call        __android_log_print
 
-    add         $8, %spl
+    add         $8, %rsp
 
     ret
 
 onInputQueueCreated:
-    sub         $8, %spl
+    sub         $8, %rsp
 
     mov         $ANDROID_LOG_INFO, %rdi
     lea         LOG_TAG(%rip), %rsi
     lea         InputQueueCreatedt(%rip), %rdx
     call        __android_log_print
 
-    add         $8, %spl
+    add         $8, %rsp
 
     ret
 
 onInputQueueDestroyed:
-    sub         $8, %spl
+    sub         $8, %rsp
 
     mov         $ANDROID_LOG_INFO, %rdi
     lea         LOG_TAG(%rip), %rsi
     lea         InputQueueDestroyedt(%rip), %rdx
     call        __android_log_print
 
-    add         $8, %spl
+    add         $8, %rsp
 
     ret
 
 onContentRectChanged:
-    sub         $8, %spl
+    sub         $8, %rsp
 
     mov         $ANDROID_LOG_INFO, %rdi
     lea         LOG_TAG(%rip), %rsi
     lea         ContentRectChangedt(%rip), %rdx
     call        __android_log_print
 
-    add         $8, %spl
+    add         $8, %rsp
 
     ret
 
 onConfigurationChanged:
-    sub         $8, %spl
+    sub         $8, %rsp
 
     mov         $ANDROID_LOG_INFO, %rdi
     lea         LOG_TAG(%rip), %rsi
     lea         ConfigurationChangedt(%rip), %rdx
     call        __android_log_print
 
-    add         $8, %spl
+    add         $8, %rsp
 
     ret
 
 onLowMemory:
-    sub         $8, %spl
+    sub         $8, %rsp
 
     mov         $ANDROID_LOG_INFO, %rdi
     lea         LOG_TAG(%rip), %rsi
     lea         LowMemoryt(%rip), %rdx
     call        __android_log_print
 
-    add         $8, %spl
+    add         $8, %rsp
 
     ret
